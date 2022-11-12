23847f3f76c767ddbd32eafac22cdceeb6fd787c
Jack Roberts <jpr2170@columbia.edu>
2022-11-11 22:54:16 -0500

    Register form works

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ import os
   # accessible as a variable in index.html:
 from sqlalchemy import *
 from sqlalchemy.pool import NullPool
-from flask import Flask, request, render_template, g, redirect, Response
+from flask import Flask,url_for, flash, request, render_template, g, redirect, Response
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'forms')
 app = Flask(__name__, template_folder=tmpl_dir)
@@ -39,7 +39,7 @@ def teardown_request(exception):
         pass
 
 @app.route('/')
-def dining_hall():
+def home_page():
     print(request.args)
     cursor = g.conn.execute("SELECT hall_name FROM dining_hall")
     dh = []
@@ -48,42 +48,60 @@ def dining_hall():
     cursor.close()
 
     context = dict(data = dh)
-    return render_template("dining_halls.html", **context)
+    return render_template("home_page.html", **context)
 
 @app.route('/plan')
 def plan(): 
     print(request.args)
-    cursor = g.conn.execute("SELECT plan_name FROM dining_plan")
+    cursor = g.conn.execute("SELECT * FROM dining_plan")
     plans = []
     for result in cursor:
-        plans.append(result['plan_name'])  # can also be accessed using result[0]
+        plans.append(result)  # can also be accessed using result[0]
     cursor.close()
 
-    context = dict(data = plans)
-
+    context = dict(plan = plans)
     return render_template("plan.html", **context)
 
-@app.route('/dining_plan')
-def dining_plan():
-    return render_template("dining_plan.html")
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method=='POST':
+        add_uni = request.form['uni']
+        add_name = request.form['name']
+        add_username = request.form['username']
+        add_year = request.form['year']
+        add_plan_name = request.form['plan_name']
+#    print(add_uni, add_name, add_username, add_year, add_plan_name)
+        g.conn.execute("INSERT INTO student(uni,name,username,year,plan_name) VALUES (%s, %s, %s, %s, %s)", add_uni, add_name, add_username, add_year, add_plan_name)
+        return redirect('/')
+    return render_template('auth.html')
 
-@app.route('/another')
-def another():
-    return render_template("another.html")
 
+@app.route('/hall page/<name>')
+def hall_page(name):
+    print(request.args)
+    hall_name = name
+    time = []
+    loc = []
+    info = []
+    cursor = g.conn.execute("SELECT * FROM dining_hall WHERE hall_name = '{}'".format(name))
+    for result in cursor:
+        time.append(result[1:3])
+        loc.append(result[4:6])
+    cursor = g.conn.execute("SELECT * FROM writes W, review R WHERE W.rid=R.rid AND W.hall_name='{}'".format(name))
+    for result in cursor:
+        info.append(result)
+    cursor.close()
+    context = dict(hall_name = hall_name, location = loc, hours = time, review = info)
+    return render_template("hall_page.html", **context)
 
 # Example of adding new data to the database
 @app.route('/add', methods=['POST'])
 def add():
   name = request.form['name']
-  g.conn.execute('INSERT INTO test(name) VALUES (%s)', name)
+  rid = g.conn.execute("SELECT MAX(rid) FROM review")
+  rid += 1
+  g.conn.execute('INSERT INTO (name) VALUES (%s)', name)
   return redirect('/')
-
-
-@app.route('/login')
-def login():
-    abort(401)
-    this_is_never_executed()
 
 
 if __name__ == "__main__":
